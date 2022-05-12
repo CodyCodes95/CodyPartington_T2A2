d2b080741a2516788c048646c063066dd39234fb
Cody Partington <codythatsme@gmail.com>
2022-05-12 13:26:46 +1000

    Relationship updated

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,6 @@ class Listing < ApplicationRecord
   belongs_to :profile, dependent: :destroy
   has_many :listing_modifications
   has_many :modifications, through: :listing_modifications
-  has_many_attached :car_images
+  has_one_attached :car_images
   accepts_nested_attributes_for :modifications, :car
 end
